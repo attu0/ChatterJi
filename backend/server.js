9667dc8fe0eef55729db55ccc3b31629042510eb
Atharv Mudse <atharvmudse@gmail.com>
2025-08-27 15:28:00 +0530

    added connection with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 const PORT = 8080;
@@ -11,29 +13,40 @@ app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is live on http://localhost:${PORT}`);
+    connnectDB();
 });
-app.post("/test", async (req, res) => {
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            model: "llama3.1:8b",
-            messages: [{
-                role: "user",
-                content: req.body.messages
-            }],
-            stream: false, 
-        })
-    };
-    try {
-        const response = await fetch("http://localhost:11434/api/chat", options);
-        const data = await response.json();
-        res.send(data.message.content);
-        console.log(data.message.content);
 
-    } catch (err) {
-        console.error(err);
+const connnectDB = async() =>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("connected with database");
+    }catch(err){
+        console.log("Failed to connect ",err);
     }
-});
+}
+
+// app.post("/test", async (req, res) => {
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             model: "llama3.1:8b",
+//             messages: [{
+//                 role: "user",
+//                 content: req.body.messages
+//             }],
+//             stream: false, 
+//         })
+//     };
+//     try {
+//         const response = await fetch("http://localhost:11434/api/chat", options);
+//         const data = await response.json();
+//         res.send(data.message.content);
+//         console.log(data.message.content);
+
+//     } catch (err) {
+//         console.error(err);
+//     }
+// });
