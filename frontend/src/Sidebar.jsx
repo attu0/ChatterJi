c6ed771340bb4c4913a14587b0fcf255e92ca305
Atharv Mudse <atharvmudse@gmail.com>
2025-08-30 05:03:13 +0530

    added css to sidebar

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,24 +1,24 @@
 import "./Sidebar.css";
 
-function Sidebar(){
-    return (
-        <section className="sidebar">
-            {/* new chat button */}
-            <button>
-                <img src="/assets/blacklogo.png" alt="logo" />
-                <i className="fa-solid fa-pen-to-square"></i>
-            </button>
-            {/* history */}
-            <ul className="history">
-                <li>History1</li>
-                <li>History1</li>
-                <li>History1</li>
-            </ul>
-            {/* sign */}
-            <div className="sign">
-                <p>By Atharv Mudse</p>
-            </div>
-        </section>
-    )
+function Sidebar() {
+  return (
+    <section className="sidebar">
+      {/* new chat button */}
+      <button>
+        <img src="/assets/blacklogo.png" alt="logo" className="logo" />
+        <i className="fa-solid fa-pen-to-square"></i>
+      </button>
+      {/* history */}
+      <ul className="history">
+        <li>History1</li>
+        <li>History1</li>
+        <li>History1</li>
+      </ul>
+      {/* sign */}
+      <div className="sign">
+        <p>By Atharv Mudse</p>
+      </div>
+    </section>
+  );
 }
 export default Sidebar;
