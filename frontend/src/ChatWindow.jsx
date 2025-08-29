72df3e85fd4378b5cd38c9194a2e91ebead9ed09
Atharv Mudse <atharvmudse@gmail.com>
2025-08-30 05:15:52 +0530

    added logic in chatwindow

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,9 +1,25 @@
-import "./ChatWindow.css"
+import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow(){
     return (
-        <div>
-            Chatwindow
+        <div className="Chatwindow">
+            <div className="navbar">
+                <span>ChatterJi <i class="fa-solid fa-arrow-down"></i></span>
+                <div className="userIconDiv" ><span><i class="fa-solid fa-user"></i></span></div>
+            </div>
+            <Chat></Chat>
+            <div className="chatInput">
+                <div className="userInput">
+                    <input placeholder="ask anything...">
+
+                    </input>
+                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    i am very dangerous i can make some mistakes that can vanish you.
+                </p>
+            </div>
         </div>
     )
 }
